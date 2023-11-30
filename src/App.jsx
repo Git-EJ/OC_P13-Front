15b65c0700a08884cd179bd3535f69322936f794
EJ <eric_j_fra@yahoo.fr>
2023-11-30 11:51:12 +0100

    1st Commit:    - Vite, Sass, React Router, ReduxJs/Toolkit, propTypes    - html from OC repo, basic implementation of html in react components    - css form OC repo, basic implementation of css with some adds

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import SignIn from "./pages/SignIn"
+import User from "./pages/User"
+import Home from "./pages/home"
+
+function App() {
+
+  return (
+    <>
+      {/* <Home /> */}
+      {/* <SignIn /> */}
+      <User />
+    </>
+  )
+}
+
+export default App
