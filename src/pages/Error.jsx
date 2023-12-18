2db25360d337fee97da40f74a1db980a2ba3e2bc
EJ <eric_j_fra@yahoo.fr>
2023-12-18 12:57:43 +0100

    CSS for error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,7 +1,9 @@
+import { useState } from "react"
 import { NavLink, useLocation } from "react-router-dom"
 
 
 const Error = () => {
+  const [errorDetails, setErrorDetails] = useState(false)
   const location = useLocation()
   const errorProps = location.state?.errorprops
 
@@ -9,13 +11,22 @@ const Error = () => {
   const errorStatusText = errorProps?.statusText || 'Not Found'
   const errorMessage = errorProps?.statusMessage || 'Server Error or Page not found'
 
+
+  const handleShowErrorDetails = () => {  
+    setErrorDetails(!errorDetails)
+  }
   
   return (
     <div className="error_container">
       <h1 className="error_code">{errorCode}</h1>
       <NavLink to="/" className='error_homebutton'>Go to Homepage</NavLink>
-      <p className="error_text">{errorStatusText}</p>
-      <p className="error_text">{errorMessage}</p>
+      <button className='error_details' onClick={(handleShowErrorDetails)}>Show Details</button>
+      {errorDetails &&
+        <>
+        <p className="error_text">response.statusText: {errorStatusText}</p>
+        <p className="error_text">response.data.message: {errorMessage}</p>
+        </>
+      }
     </div>
   )
 }
